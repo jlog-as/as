11e4d9bfdb70f7d31ae8fa6f7b321a603f1209af
as <justlog12@gmail.com>
2024-11-24 22:59:46 +0530

    up

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Recipe Sharing App header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/Recipe Sharing App/i);
+  expect(headerElement).toBeInTheDocument();
 });
